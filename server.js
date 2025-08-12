cc1f7a55b98e8d2094e2906d5b85be04e4d854fe
softwareInkhub <software.inkhub@gmail.com>
2025-08-12 17:57:10 +0530

    Force CORS fix with preflight handling

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(cors({
   origin: true, // Allow all origins for now to debug
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
 // Request logging middleware
